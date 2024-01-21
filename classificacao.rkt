3f7a76d3f5b762e4f7a72b007616dd731938da26
Luca <lucafidideus@gmail.com>
2024-01-20 23:34:03 -0300

    so falta formatar

--- classificacao.rkt
+++ classificacao.rkt
@@ -49,47 +49,154 @@
 (define (encontra-times resultados)
   (remove-duplicates (encontra-times1 resultados)))
 
-;;
-(define (calcula-desempenhos times resultados)
-  (define desemp ((resultado-time1 (first resultados)) 0 0 0))
+;;Calcula os pontos de um time no campeonato
+(define (calcula-pontos-aux times resultados)
   (cond
+    [(empty? resultados) empty]
+    [(empty? times) empty]
     [(equal? (first times) (resultado-time1 (first resultados)))
      (cond
        [(> (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados)))
-        (+ 3 (desempenho-pontos desemp))
-        (+ 1 (desempenho-vitorias desemp))
-        (+ (- (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados))) (desempenho-saldo-gols desemp))]
+        (cons 3 (calcula-pontos-aux times (rest resultados)))]
        [(< (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados)))
-        (+ (- (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados))) (desempenho-saldo-gols desemp))]
-       [else
-        (+ 1 (desempenho-pontos desemp))
-        (+ 1 (desempenho-vitorias desemp))
-        (+ (- (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados))) (desempenho-saldo-gols desemp))]
-       
-       [else (calcula-desempenhos times (rest resultados))])
+        (calcula-pontos-aux times (rest resultados))]
+       [(= (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados)))
+        (cons 1 (calcula-pontos-aux times (rest resultados)))]
+       [else (calcula-pontos-aux (rest times) resultados)])]
+    [(equal? (first times) (resultado-time2 (first resultados)))
+     (cond
+       [(> (resultado-gols2 (first resultados)) (resultado-gols1 (first resultados)))
+        (cons 3 (calcula-pontos-aux times (rest resultados)))]
+       [(< (resultado-gols2 (first resultados)) (resultado-gols1 (first resultados)))
+        (calcula-pontos-aux times (rest resultados))]
+       [(= (resultado-gols2 (first resultados)) (resultado-gols1 (first resultados)))
+        (cons 1 (calcula-pontos-aux times (rest resultados)))]
+       [else (cons (calcula-pontos-aux (rest times) resultados))])]
+    [else (calcula-pontos-aux times (rest resultados))]))
+
+(define (calcula-pontos times resultados)
+  (cond
+    [(empty? times) empty]
+    [else
+     (cons (foldr + 0 (calcula-pontos-aux times resultados)) (calcula-pontos (rest times) resultados))]))
+
+;;Calcula as vitórias de um time no campeonato
+(define (calcula-vitorias-aux times resultados)
+  (cond
+    [(empty? resultados) empty]
+    [(empty? times) empty]
+    [(equal? (first times) (resultado-time1 (first resultados)))
+     (cond
+       [(> (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados)))
+        (cons 1 (calcula-vitorias-aux times (rest resultados)))]
+       [(< (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados)))
+        (calcula-vitorias-aux times (rest resultados))]
+       [(= (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados)))
+        (calcula-vitorias-aux times (rest resultados))]
+       [else (calcula-vitorias-aux (rest times) resultados)])]
+    [(equal? (first times) (resultado-time2 (first resultados)))
+     (cond
+       [(> (resultado-gols2 (first resultados)) (resultado-gols1 (first resultados)))
+        (cons 1 (calcula-vitorias-aux times (rest resultados)))]
+       [(< (resultado-gols2 (first resultados)) (resultado-gols1 (first resultados)))
+        (calcula-vitorias-aux times (rest resultados))]
+       [(= (resultado-gols2 (first resultados)) (resultado-gols1 (first resultados)))
+        (calcula-vitorias-aux times (rest resultados))]
+       [else (cons (calcula-vitorias-aux (rest times) resultados))])]
+    [else (calcula-vitorias-aux times (rest resultados))]))
+
+(define (calcula-vitorias times resultados)
+  (cond
+    [(empty? times) empty]
+    [else
+     (cons (foldr + 0 (calcula-vitorias-aux times resultados)) (calcula-vitorias (rest times) resultados))]))
+
+;;Calcula o saldo de gols de um time
+(define (calcula-sg-aux times resultados)
+  (cond
+    [(empty? resultados) empty]
+    [(empty? times) empty]
+    [(equal? (first times) (resultado-time1 (first resultados)))
+     (cons (- (resultado-gols1 (first resultados)) (resultado-gols2 (first resultados))) (calcula-sg-aux times (rest resultados)))]
+    [(equal? (first times) (resultado-time2 (first resultados)))
+     (cons (- (resultado-gols2 (first resultados)) (resultado-gols1 (first resultados))) (calcula-sg-aux times (rest resultados)))]
+    [else (calcula-sg-aux times (rest resultados))]))
+
+(define (calcula-sg times resultados)
+  (cond
+    [(empty? times) empty]
+    [else
+     (cons (foldr + 0 (calcula-sg-aux times resultados)) (calcula-sg (rest times) resultados))]))
+    
+;;Calcula os desempenhos de cada time
+(define (calcula-desempenhos times resultados)
+  (cond
+    [(empty? times) empty]
+    [else
+     (define desemp (desempenho (first times)
+                                (first (calcula-pontos times resultados))
+                                (first (calcula-vitorias times resultados))
+                                (first (calcula-sg times resultados))))
+     (cons desemp (calcula-desempenhos (rest times) resultados))]))
 
 
+;;Classifica os desempenhos de acordo com estes parâmetros:
+;;Primeiro: Pontos
+;;Segundo: Número de vitórias
+;;Terceiro: Saldo de gols
+;;Quarto: Ordem alfabética
+(define (compara-times desempenho1 desempenho2)
+  (cond
+  [(> (desempenho-pontos desempenho1) (desempenho-pontos desempenho2)) #t]
+  [(< (desempenho-pontos desempenho1) (desempenho-pontos desempenho2)) #f]
+  [(> (desempenho-vitorias desempenho1) (desempenho-vitorias desempenho2)) #t]
+  [(< (desempenho-vitorias desempenho1) (desempenho-vitorias desempenho2)) #f]
+  [(> (desempenho-saldo-gols desempenho1) (desempenho-saldo-gols desempenho2)) #t]
+  [(< (desempenho-saldo-gols desempenho1) (desempenho-saldo-gols desempenho2)) #f]
+  [(string<=? (desempenho-time desempenho1) (desempenho-time desempenho2)) #t]
+  [else #f]))
+
+(define (insere-ordenado desempenho desempenhos comparacao)
+  (cond
+    [(empty? desempenhos) (list desempenho)]
+    [(comparacao desempenho (first desempenhos))
+     (cons desempenho desempenhos)]
+    [else
+     (cons (first desempenhos) (insere-ordenado desempenho (rest desempenhos) comparacao))]))
+
+(define (classifica desempenhos)
+  (if (empty? desempenhos)
+      '()
+      (insere-ordenado (first desempenhos)
+                       (classifica (rest desempenhos))
+                       compara-times)))
+
+;;Transforma a lista de desempenhos em uma lista de strings
+(define (desempenho->string desempenho)
+  (cond
+    [(empty? desempenho) empty]
+    [else
+     (string-append (desempenho-time desempenho)
+                    (make-string (- (string-length (desempenho-time desempenho)) alinhamento) #\space)
+                    (number->string (desempenho-pontos desempenho))
+                    (make-string (- alinhamento (string-length (number->string (desempenho-pontos desempenho)))) #\space)
+                    (number->string (desempenho-vitorias desempenho))
+                    (make-string (- alinhamento (string-length (number->string (desempenho-vitorias desempenho)))) #\space)
+                    (number->string (desempenho-saldo-gols desempenho)))
      ]))
-    
-    
 
-
-
-
+(define alinhamento 5)
 
 ;; ListaString -> ListaString
-;;(define (classifica-times sresultados)
-
-(define resultados (map string->resultado1 sresultados))
+(define (classifica-times sresultados)
+  (define resultados (map string->resultado1 sresultados))
   
-(define times (encontra-times resultados))
+  (define times (encontra-times resultados))
 
-  ;;(define desempenhos (calcula-desempenhos times resultados))
+  (define desempenhos (calcula-desempenhos times resultados))
 
-  ;;(define classificacao (classifica desempenhos))
+  (define classificacao (classifica desempenhos))
 
-  ;;(map desempenho->string classificacao))
-
+  (map desempenho->string classificacao))
 
 ;;(display-lines (classifica-times (port->lines)))
-  
